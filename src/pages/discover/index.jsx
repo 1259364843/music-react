94d44e677eb89918afd75a1a6730d4d4bacac619
sch <1259364843@qq.com>
2022-02-12 10:01:55 +0800

    discover子组件搭建和配置路由

--- src/pages/discover/index.jsx
+++ src/pages/discover/index.jsx
@@ -1,9 +1,35 @@
 import React, { memo } from 'react'
 
-export default memo(function Discover() {
+// 数据
+import { dicoverMenu } from "@/common/local-data";
+
+import { NavLink } from 'react-router-dom';
+import { renderRoutes } from "react-router-config";
+// 样式
+import {
+  DiscoverWrapper,
+  TopMenu
+} from './style';
+export default memo(function Discover(props) {
+  /*路由对象*/
+  const { route } = props;
   return (
-    <div>
-      <h2>Discover</h2>
-    </div>
+    <DiscoverWrapper>
+      <div className='top'>
+        <TopMenu className='wrap-v1'>
+          {
+            dicoverMenu.map((item, index) => {
+              return (
+                <div className="item" key={item.title}>
+                  {/*路由跳转*/}
+                  <NavLink to={item.link}>{item.title}</NavLink>
+                </div>
+              )
+            })
+          }
+        </TopMenu>
+      </div>
+      {renderRoutes(route.routes)}
+    </DiscoverWrapper>
   )
 })
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ import { Redirect } from "react-router";
 import Discover from "@/pages/discover";
 import Friend from "@/pages/friend";
 import Mine from "@/pages/mine";
+// 子组件
+import Recommend from "@/pages/discover/c-pages/recommend";
+import Ranking from "@/pages/discover/c-pages/ranking";
+import Songs from "@/pages/discover/c-pages/songs";
+import Djradio from "@/pages/discover/c-pages/djradio";
+import Artist from "@/pages/discover/c-pages/artist";
+import Album from "@/pages/discover/c-pages/album";
 
 const routes = [
   {
@@ -14,6 +21,34 @@ const routes = [
   {
     path: "/discover",
     component: Discover,
+    // 子路由
+    routes: [
+      {
+        path: "/discover/recommend",
+        component: Recommend
+      },
+      {
+        path: "/discover/ranking",
+        component: Ranking
+      },
+      {
+        path: "/discover/songs",
+        component: Songs
+      },
+      {
+        path: "/discover/djradio",
+        exact: true,
+        component: Djradio
+      },
+      {
+        path: "/discover/artist",
+        component: Artist
+      },
+      {
+        path: "/discover/album",
+        component: Album
+      }
+    ]
   },
   {
     path: "/mine",
